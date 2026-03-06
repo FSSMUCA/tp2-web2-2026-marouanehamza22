30248a5988044023de757d749c7383764f012257
Marouane Hamza <m.hamza6741@uca.ac.ma>
2026-03-06 12:13:22 +0000

    add TP2 JavaScript exercices 1-8

---
+++ exercice7.js
@@ -0,0 +1,46 @@
+let nomProduit = "Clavier mécanique";
+let prix = 89.99;
+let quantite = 3;
+let codePromo = null;
+let reductionPourcentage = 10;
+let estMembre = true;
+let soldeCompte = 250;
+
+let sousTotal = prix * quantite;
+console.log("Sous-total :", sousTotal);
+
+let reduction = 0;
+
+if(codePromo !== null && estMembre){
+reduction = sousTotal * reductionPourcentage / 100;
+}
+
+console.log("Reduction :", reduction);
+
+let total = sousTotal - reduction;
+console.log("Total :", total);
+
+let statut = "";
+
+if(soldeCompte >= total){
+statut = "Paiement accepté";
+}else{
+statut = "Solde insuffisant";
+}
+
+console.log(statut);
+
+if(statut === "Paiement accepté"){
+soldeCompte = soldeCompte - total;
+}
+
+console.log("===== RÉCAPITULATIF =====");
+console.log("Produit   :", nomProduit);
+console.log("Quantité  :", quantite);
+console.log("Prix unit.:", prix,"MAD");
+console.log("Sous-total:", sousTotal,"MAD");
+console.log("Réduction :", reduction,"MAD");
+console.log("Total     :", total,"MAD");
+console.log("Statut    :", statut);
+console.log("Solde     :", soldeCompte,"MAD");
+console.log("=========================");
